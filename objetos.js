a57c921700b54bb9d6498e0e4d7cd50a00ce17b2
Gabriela <mariaglinares25@gmail.com>
2026-06-05 15:18:25 -0500

    Agregando archivos js

---
+++ objetos.js
@@ -0,0 +1,15 @@
+const personas = {
+    nombre: "Juan",
+    apellido: "Pérez",
+    edad: 30,
+}
+
+console.log(personas.nombre);
+console.log(personas.apellido);
+console.log(personas.edad);
+console.log(personas);
+
+
+//personas.nombre = "María";
+//personas.apellido = "Gómez";
+//personas.edad = 25;
